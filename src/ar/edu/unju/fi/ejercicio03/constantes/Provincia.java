3d86d84f8dad5e070d850122a7d0bd38c005eba1
Javi <Javi@DESKTOP-JM6NVPK>
2024-04-27 00:39:31 -0300

    Ejercicio 3 terminado

--- src/ar/edu/unju/fi/ejercicio03/constantes/Provincia.java
+++ src/ar/edu/unju/fi/ejercicio03/constantes/Provincia.java
@@ -1,34 +1,29 @@
 package ar.edu.unju.fi.ejercicio03.constantes;
 
 public enum Provincia {
-	JUJUY,SALTA,TUCUMAN,LA_RIOJA,SANTIAGO_DEL_ESTERO;
+	JUJUY(700881,53244.2),SALTA(1424397,155340.5),TUCUMAN(1694656,22592.1),LA_RIOJA(393531,91493.7),
+	SANTIAGO_DEL_ESTERO(978313,136934.3);
 	
-	private long cantidadPoblacion;
+	private double cantidadPoblacion;
 	private double superficie;
 	
 	private Provincia() {
 		// TODO Auto-generated constructor stub
 	}
-	private Provincia(long cantidadPoblacion, double superficie) {
+	private Provincia(double cantidadPoblacion, double superficie) {
 		this.cantidadPoblacion = cantidadPoblacion;
 		this.superficie = superficie;
 	}
 
-	public long getCantidadPoblacion() {
+	public double getCantidadPoblacion() {
 		return cantidadPoblacion;
-	}
-
-	public void setCantidadPoblacion(long cantidadPoblacion) {
-		this.cantidadPoblacion = cantidadPoblacion;
 	}
 
 	public double getSuperficie() {
 		return superficie;
 	}
-
-	public void setSuperficie(double superficie) {
-		this.superficie = superficie;
-	} 
 	
-	
+	public Double calcularDensidad() {
+		return this.cantidadPoblacion/this.superficie;
+	}
 }
--- src/ar/edu/unju/fi/ejercicio03/main/Main.java
+++ src/ar/edu/unju/fi/ejercicio03/main/Main.java
@@ -1,10 +1,52 @@
 package ar.edu.unju.fi.ejercicio03.main;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio03.constantes.Provincia;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scanner=new Scanner(System.in);
+		List<Double> densidades = new ArrayList();
+			Provincia provincia = Provincia.JUJUY;
+			
+			System.out.println("\n Provincia : "+ provincia);
+			System.out.println("Cantidad Poblacion :"+provincia.getCantidadPoblacion());
+			System.out.println("Superficie: "+provincia.getSuperficie());
+			densidades.add(provincia.calcularDensidad());
+			System.out.println("Densidad: "+densidades.get(0));
+			
+			provincia= Provincia.SALTA;
+			System.out.println("\n Provincia : "+ provincia);
+			System.out.println("Cantidad Poblacion :"+provincia.getCantidadPoblacion());
+			System.out.println("Superficie: "+provincia.getSuperficie());
+			densidades.add(provincia.calcularDensidad());
+			System.out.println("Densidad: "+densidades.get(1));
+			
+			provincia= Provincia.TUCUMAN;
+			System.out.println("\n Provincia : "+ provincia);
+			System.out.println("Cantidad Poblacion :"+provincia.getCantidadPoblacion());
+			System.out.println("Superficie: "+provincia.getSuperficie());
+			densidades.add(provincia.calcularDensidad());
+			System.out.println("Densidad: "+densidades.get(2));
+			
+			provincia= Provincia.LA_RIOJA;
+			System.out.println("\n Provincia : "+ provincia);
+			System.out.println("Cantidad Poblacion :"+provincia.getCantidadPoblacion());
+			System.out.println("Superficie: "+provincia.getSuperficie());
+			densidades.add(provincia.calcularDensidad());
+			System.out.println("Densidad: "+densidades.get(3));
+		
+			provincia= Provincia.SANTIAGO_DEL_ESTERO;
+			System.out.println("\n Provincia : "+ provincia);
+			System.out.println("Cantidad Poblacion :"+provincia.getCantidadPoblacion());
+			System.out.println("Superficie: "+provincia.getSuperficie());
+			densidades.add(provincia.calcularDensidad());
+			System.out.println("Densidad: "+densidades.get(4));
+			
 	}
-
 }
